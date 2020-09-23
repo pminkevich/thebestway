fb1614d1fca6396bfa969b595e3f92b21adf875b
Pablo Minkevich <pminkevich@gmail.com>
2020-09-23 14:52:06 -0300

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule, Router } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes:Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  //hacer redirec
+  //{path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    RouterModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBxEaU04N6avVBC5NRJ1B_BQxlII6fR5zc",
+    authDomain: "thebestway-fc5e3.firebaseapp.com",
+    databaseURL: "https://thebestway-fc5e3.firebaseio.com",
+    projectId: "thebestway-fc5e3",
+    storageBucket: "thebestway-fc5e3.appspot.com",
+    messagingSenderId: "335017167412",
+    appId: "1:335017167412:web:e8b5a99d1080f97bdf1861"
+}
 };
 
 /*
